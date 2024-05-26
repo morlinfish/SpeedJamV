929fddae44f7a270fd5420dcae81cbee288356b3
seeacois <seeacois@gmail.com>
2024-05-26 00:29:02 -0400

    A:>KFJUBSAEKL:IIUFGBIO:U

--- HeLLHole/Assets/Scripts/GraysonScripts/SDPlatform.cs
+++ HeLLHole/Assets/Scripts/GraysonScripts/SDPlatform.cs
@@ -6,13 +6,16 @@ public class SDPlatform : MonoBehaviour
 {
     public GameObject sdPlatform;
     public GameObject spawnPoint;
+    public GameObject player;
+    public Transform cameraPos;
+    public GameObject spawnPoint2;
     public int sdPlatformUnused = 1;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -43,5 +46,22 @@ public class SDPlatform : MonoBehaviour
         }
 
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "ENDLEVEL1")
+        {
+            spawnPoint.transform.position = spawnPoint2.transform.position;
+            gameObject.transform.position = spawnPoint2.transform.position;
+            Camera.main.transform.Translate(23, 5, 0);
+            sdPlatformUnused = 1;
+
+            GameObject[] myTriggers = GameObject.FindGameObjectsWithTag("Platform");
+            foreach (GameObject trigger in myTriggers)
+            {
+                Destroy(trigger);
+            }
+        }
     }
 }
